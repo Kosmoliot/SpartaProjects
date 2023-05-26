610902c3473df7939b1db1686a0ebaf3be1f274e
Miguel <michail.pliuscik@gmail.com>
2023-05-26 10:28:34 +0100

    pokemon working version

--- SpartaProjects/pokemon_game/pokemon.py
+++ SpartaProjects/pokemon_game/pokemon.py
@@ -1,110 +1,128 @@
-import json
+from tabulate import tabulate
+from urllib.error import HTTPError
 import requests
 import random
-from tabulate import tabulate
 
 
 class Pokemon():
     def __init__(self, pok_name) -> None:
         self.pok_name = pok_name
         self.full_info = get_resource(pok_name)
+        self.moves = get_moves(self.full_info)
+        self.stats = get_stats(self.full_info)
 
 
-def get_resource(name): 
+def get_resource(name):
     pokemon_api = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
-    return pokemon_api.json()
+    if pokemon_api.status_code == requests.codes.ok:
+        return pokemon_api.json()
+    else:
+        print("There's no such pokemon, please try again.")
 
 
-def get_moves():
+def get_moves(info):
     move_list = {}
-    with open("pokemon.json") as jsonfile:
-        pok_data = json.load(jsonfile)
-    for key in pok_data.keys():
-        move_list[key] = []
-        for move in pok_data[key]["moves"]:
-            move_list[key].append(move["move"]["name"])
+    for move in info["moves"]:
+        move_list[move["move"]["name"]] = get_move_dmg(move["move"]["url"])
+        if len(move_list) == 5:
+            break
     return move_list
 
 
-def get_stats():
+def get_move_dmg(url):
+    pokemon_api = requests.get(url)
+    if pokemon_api.status_code == requests.codes.ok:
+        return pokemon_api.json()["power"]
+    else:
+        print("There's no such pokemon, please try again.")
+    
+
+def get_stats(info):
     stats = {}
-    with open("pokemon.json") as jsonfile:
-        pok_data = json.load(jsonfile)
-    for key in pok_data.keys():
-        stats[key] = []
-        for stat in pok_data[key]["stats"]:
-            stats[key].append({stat["stat"]["name"]: stat["base_stat"]})
+    for stat in info["stats"]:
+        stats[stat["stat"]["name"]] = stat["base_stat"]
     return stats
 
 
-def create_combatants(combatant1, combatant2):
-    combatants = {combatant1: Pokemon(combatant1).full_info, combatant2: Pokemon(combatant2).full_info}
-    with open("pokemon.json", "w") as file:
-        json.dump(combatants, file)
-        
+def print_info(pokemon, info, stat, value):
+    print(f"\n#========== {pokemon.pok_name.capitalize()} {stat} ==========#")
+    table = tabulate([(key, value) for key, value in info.items()], headers=[stat, value], tablefmt="psql")
+    print(f"{table}\n")
+    
 
-def print_stats():
-    # Convert dictionary to a tuples
-    pok_stats = get_stats()
-    for pokemon in pok_stats:
-        print(f"\n#========== {pokemon.capitalize()} stats ==========#")
-        table = tabulate([(k, v) for d in pok_stats[pokemon] for k, v in d.items()], headers=["Key", "Value"], tablefmt="psql")
-        print(f"{table}\n")
-  
+def print_stats(pokemon):
+    return print_info(pokemon, pokemon.stats, "Stat", "Value")
+
+
+def print_moves(pokemon):
+   return print_info(pokemon, pokemon.moves, "Moves", "Damage")
+
 
 def enter_pokemon():
     usr_input = input("Please type in your pokemon name: ")
     if requests.get(f"https://pokeapi.co/api/v2/pokemon/{usr_input}").status_code == requests.codes.ok:
-        return usr_input
+        return Pokemon(usr_input)
     else:
         print("There's no such pokemon, please try again.")
 
 
 def random_pokemon():
     pokemon_list = ["ditto", "kakuna", "pidgey", "snorlax", "piplup"]
-    return random.choice(pokemon_list)
+    return Pokemon(random.choice(pokemon_list))
+    
 
 
-def attack():
-    pass
-
-
-def super_attack():
-    pass
-
-
-def defense():
-    pass
-
-
-def super_defense():
-    pass
+def attack(pokemon, opponent, user_attack):
+    y_move = list(pokemon.moves.keys())[user_attack-1]
+    opponent.stats["hp"] -= pokemon.moves[y_move]
+    print(f"\nYou have dealt {pokemon.moves[y_move]} damage")
+    o_move = random.choice(list(opponent.moves.values()))
+    pokemon.stats["hp"] -= o_move
+    print(f"You have received {o_move}")
 
     
 #===============Fight Menu===============#
 
-def fight_menu():
+def fight_menu(pokemon, opponent):
     while True:
+        if pokemon.stats["hp"] <= 0:
+            print("\nYou won, well done!")
+            break
+        elif opponent.stats["hp"] <= 0:
+            print("\nYou lost, don't be upset. Try again!")
+            break
         try:
-
-            user_choice = int(input("""\nWhat would you like to do:
-1\t-\tView stats
-2\t-\tChoose a move
-0\t-\tRun away
+            user_choice = int(input(f"""\nChoose an attack from the table :
+1\t-\t{list(pokemon.moves.keys())[0]}
+2\t-\t{list(pokemon.moves.keys())[1]}
+3\t-\t{list(pokemon.moves.keys())[2]}
+4\t-\t{list(pokemon.moves.keys())[3]}
+5\t-\t{list(pokemon.moves.keys())[4]}
+\n0\t-\tGive up
 : """))
             if user_choice == 1:
-                print_stats()         
+                attack(pokemon, opponent, 1)
                 
             elif user_choice == 2:
-                print(get_moves())   
+                attack(pokemon, opponent, 2)
+            
+            elif user_choice == 3:
+                attack(pokemon, opponent, 3)
+                
+            elif user_choice == 4:
+                attack(pokemon, opponent, 4)
+                
+            elif user_choice == 5:
+                attack(pokemon, opponent, 5)
             
             elif user_choice == 0:
-                print("\nCome back you COWARD!")
+                print("\nI thought I tought you better!")
                 break
             else:
                 print("\nOops - incorrect input")
         except ValueError:
             print("\nEntry should be a number from the menu options.\n")
+    
 
 
 #===============Choose pokemon===============#
@@ -132,17 +150,32 @@ def choose_pokemon():
 
 #===============Main Menu===============#
 
+def main_menu(pokemon):
+    while True:
+        try:
 
-# choose_pokemon()
+            user_choice = int(input("""\nWhat would you like to do:
+1\t-\tView stats
+2\t-\tPrint moves
+3\t-\tAttack
+0\t-\tRun away
+: """))
+            if user_choice == 1:
+                print_stats(pokemon)  
+                
+            elif user_choice == 2:
+                print_moves(pokemon)
+                
+            elif user_choice == 3:
+                fight_menu(pokemon, random_pokemon())
+            
+            elif user_choice == 0:
+                print("\nCome back you COWARD!")
+                break
+            else:
+                print("\nOops - incorrect input")
+        except ValueError:
+            print("\nEntry should be a number from the menu options.\n")
 
-# first_item = next(iter(get_stats().items()))[0]
-# print(first_item[0])
 
-
-def main_menu():
-    create_combatants(choose_pokemon(), random_pokemon())
-    fight_menu()
-    
-# main_menu()
-
-print(get_moves())
+main_menu(choose_pokemon())
--- SpartaProjects/pokemon_game/test_pokemon.py
+++ SpartaProjects/pokemon_game/test_pokemon.py
@@ -1,100 +1,36 @@
 import unittest
-from restaurant import Table
+from pokemon import Pokemon
 
 
 class UnitTests(unittest.TestCase):
     def setUp(self) -> None:
-        self.table02 = Table(2)
-        self.table05 = Table(5)
-        self.table06 = Table(6)
+        self.pok1 = Pokemon("pikachu")
+        self.pok2 = Pokemon("mew")
+        self.pok3 = Pokemon("bulbasaur")
 
-    def test_order(self):
-        self.table02.order('Food', 10.00, 3)
-        actual = self.table02.bill
-        expected = [{
-            'item': 'Food',
-            'price': 10.00,
-            'quantity': 3}]
+    def test_get_resource(self):
+        result = self.pok1.full_info
+        self.assertIsInstance(result, dict, 
+        "Expected `get_resource` method to create a dictionary in Pokemon instance variable."
+        )
+        
+    def test_get_moves(self):
+        actual = self.pok2.moves
+        expected = {'pound': 40, 'mega-punch': 80, 'pay-day': 40, 
+        'fire-punch': 75, 'ice-punch': 75}
         self.assertEqual(
             actual, expected,
-            "Expected `order` method to create object in bill instance variable."
+            "Expected `get_moves` method to create a list of 5 or less moves in instance variable."
         )
-
-    def test_order_no_quantity(self):
-        self.table02.order('Food', 10.00)
-        actual = self.table02.bill
-        expected = [{
-            'item': 'Food',
-            'price': 10.00,
-            'quantity': 1}]
+        
+    def test_get_stats(self):
+        actual = self.pok3.stats
+        expected = {'hp': 45, 'attack': 49, 'defense': 49, 'special-attack': 65,
+        'special-defense': 65, 'speed': 45}
         self.assertEqual(
             actual, expected,
-            "Expected calling `order` with no quantity to create item with quantity 1."
+            "Expected `get_stats` method to create a list of stats in instance variable."
         )
-
-    def test_remove(self):
-        self.table02.order('Food', 10.00, 5)
-        self.table02.remove('Food', 10.00, 3)
-        actual = self.table02.bill
-        expected = [{
-            'item': 'Food',
-            'price': 10.00,
-            'quantity': 2}]
-        self.assertEqual(
-            actual, expected,
-            "Expected `remove` method to reduce quantity of existing item."
-        )
-
-    def test_remove_no_item(self):
-        self.table02.order('Food', 10.00, 1)
-        result = self.table02.remove('Food', 11.00, 1)
-        self.assertFalse(
-            result,
-            "Expected `remove` method with item and price not present in bill to return False."
-        )
-        result = self.table02.remove('Food2', 10.00, 1)
-        self.assertFalse(
-            result,
-            "Expected `remove` method with item and price not present in bill to return False."
-        )
-
-    def test_get_subtotal(self):
-        self.table02.order('Food1', 10.00, 3)
-        self.table02.order('Food2', 20.00, 1)
-        self.table02.order('Food3', 0.50, 1)
-        actual = self.table02.get_subtotal()
-        expected = 50.50
-        self.assertAlmostEqual(
-            actual, expected, 2,
-            "Expected balance to 2 decimal places to be 50.50."
-        )
-
-    def test_get_total(self):
-        self.table05.order('Food1', 10.00, 3)
-        self.table05.order('Food2', 20.00, 1)
-        self.table05.order('Food3', 0.60, 1)
-        actual = self.table05.get_total(0.15)
-        expected = {
-            'Sub Total': '£50.60',
-            'Service Charge': '£7.59',
-            'Total': '£58.19'
-        }
-        self.assertEqual(
-            actual, expected,
-            "Expected different dictionary from `get_total`."
-        )
-
-    def test_split_bill(self):
-        self.table06.order('Food1', 20.00, 3)
-        self.table06.order('Food2', 10.00, 1)
-        self.table06.order('Food3', 3.20, 1)
-        actual = self.table06.split_bill()
-        expected = 12.20
-        self.assertAlmostEqual(
-            actual, expected, 2,
-            "Expected `split_bill` to return 12.20 to 2 decimal places."
-        )
-
 
 if __name__ == "__main__":
     unittest.main()
