027a47c9fee93ecc53c2f644adee5486ad319343
Miguel <michail.pliuscik@gmail.com>
2023-06-20 14:51:57 +0100

    final Sparta Academy project

---
+++ SpartaProjects/final_project/test.py
@@ -0,0 +1,41 @@
+import boto3
+import pandas as pd
+
+# s3_client = boto3.client('s3')
+
+# obj = s3_client.get_object(Bucket='data-eng-228-final-project', Key='Talent/10383.json')
+# data = json.load(obj['Body'])
+
+
+# # Read the file from S3
+# response = s3_client.get_object(Bucket='data-eng-228-final-project', Key="Talent/Sparta Day 1 August 2019.txt")
+# content = response['Body'].read().decode('utf-8')
+
+#Display the file contents
+# print(content)
+# print(data)
+
+
+# def show(key:str):
+#     obj = s3_client.get_object(Bucket='data-eng-228-final-project', Key=key)
+#     filetype = key[key.index('.'):]
+#     if filetype == '.csv':
+#         data = pd.read_csv(obj['Body'])
+#         return data
+#     elif filetype == '.json':
+#         data = json.load(obj['Body'])
+#         print(data)
+#     elif filetype == '.txt':
+#         print(obj['Body'].read().decode('utf-8'))
+
+# df = show("Talent/Sept2019Applicants.csv")
+
+# for column in df.columns:
+#     print(column)
+
+
+# Load JSON data into a DataFrame with normalization
+data = pd.read_json('10383.json')
+
+# Display the DataFrame
+print(data)
